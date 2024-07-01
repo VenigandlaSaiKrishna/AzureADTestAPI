648bcb421b81d8158b7b8918e591d2d7d15957ea
sai krishna <saikrishnasai926@gmail.com>
2024-07-02 01:02:24 +0530

    change

---
+++ Program.cs
@@ -0,0 +1,84 @@
+using AzureADTestAPI.SwaggerAuthorizeCheckOperationFilter;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(options =>
+    {
+        builder.Configuration.Bind("ArticleMasterEntraAD", options);
+    }, options =>
+    {
+        builder.Configuration.Bind("ArticleMasterEntraAD", options);
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    //use this option for swagger
+    options.AddPolicy("AuthPolicy", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scp", "API.ReadWrite");
+    });
+    //use this option for exposing to Client App's
+    options.AddPolicy("Admin", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("User", policy => policy.RequireRole("User"));
+});
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Test API", Version = "v1" });
+        c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.OAuth2,
+            Flows = new OpenApiOAuthFlows
+            {
+                AuthorizationCode = new OpenApiOAuthFlow
+                {
+                    AuthorizationUrl = new Uri($"{builder.Configuration["ArticleMasterEntraAD:Instance"]}{builder.Configuration["ArticleMasterEntraAD:TenantId"]}/oauth2/v2.0/authorize"),
+                    TokenUrl = new Uri($"{builder.Configuration["ArticleMasterEntraAD:Instance"]}{builder.Configuration["ArticleMasterEntraAD:TenantId"]}/oauth2/v2.0/token"),
+                    RefreshUrl = new Uri($"{builder.Configuration["ArticleMasterEntraAD:Instance"]}{builder.Configuration["ArticleMasterEntraAD:TenantId"]}/oauth2/v2.0/token"),
+                    Scopes = new Dictionary<string, string>
+                    {
+                        { $"{builder.Configuration["ArticleMasterEntraAD:Audience"]}/.default", "Access the API" }
+                    }
+                }
+            }
+        });
+        c.OperationFilter<SwaggerAuthorizeCheckOperationFilter>();
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Arcticle Master API v1");
+        //for developer use only
+        c.OAuthClientId(builder.Configuration["ArticleMasterEntraAD:ClientId"]);
+        c.OAuthUsePkce();
+    });
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
---
+++ SwaggerAuthorizeCheckOperationFilter/SwaggerAuthorizeCheckOperationFilter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace AzureADTestAPI.SwaggerAuthorizeCheckOperationFilter
+{
+    public class SwaggerAuthorizeCheckOperationFilter : IOperationFilter
+    {
+        private readonly IConfiguration _config;
+
+        public SwaggerAuthorizeCheckOperationFilter(IConfiguration config)
+        {
+            _config = config;
+        }
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var hasAuthorize = context.MethodInfo.DeclaringType.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any()
+                               || context.MethodInfo.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any();
+
+            if (hasAuthorize)
+            {
+                operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
+                operation.Responses.Add("403", new OpenApiResponse { Description = "Forbidden" });
+
+                var oAuthScheme = new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+                };
+
+                operation.Security = new List<OpenApiSecurityRequirement>
+            {
+                new OpenApiSecurityRequirement
+                {
+                    [ oAuthScheme ] = new[] { _config["ArticleMasterEntraAD:Scopes"] },
+                }
+            };
+            }
+        }
+    }
+
+}
